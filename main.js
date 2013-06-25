5ba5230cf273ba7db647a3570e9a8887a8fcf850
Max Nikolenko <maxim.nikolenko@gmail.com>
2013-06-25 22:51:29 +0300

    Remove the page number from the url

--- main.js
+++ main.js
@@ -1,26 +1,19 @@
 var ITEMS_PER_PAGE = 30;
 
-// http://stackoverflow.com/questions/901115/how-can-i-get-query-string-values/901144#901144
-function getParameterByName(name) {
-    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
-    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-        results = regex.exec(location.search);
-    return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
-}
+var page = 0;
 
 $(document).ready(function () {
-  var page = parseInt( getParameterByName('page') ) || 1;
 
   var hnsearch_api = "http://api.thriftdb.com/api.hnsearch.com/items/" + 
   "_search?filter[fields][points]=[100%20TO%20*]&sortby=create_ts%20desc&start=" + 
-  ((page - 1) * ITEMS_PER_PAGE) + "&limit=" + ITEMS_PER_PAGE + "&callback=?";
+  (page * ITEMS_PER_PAGE) + "&limit=" + ITEMS_PER_PAGE + "&callback=?";
 
   var items_list = $('#items');
 
   $.getJSON( hnsearch_api )
     .done(function( data ) {
       // set the starting point of the ordered list
-      items_list.attr('start', ( (page - 1) * ITEMS_PER_PAGE ) + 1);
+      items_list.attr('start', (page * ITEMS_PER_PAGE) + 1);
       
       var results = data.results;
       var item = null;
