17452500eadd76ee3779d0160443f962120fa9fe
Max Nikolenko <maxim.nikolenko@gmail.com>
2013-06-26 01:01:07 +0300

    Add details about items

--- main.js
+++ main.js
@@ -44,7 +44,12 @@ function load_items( page )
       var items = [];
       for (var i = 0; i < results.length; i++) {
         item = results[i].item;
-        items.push('<li><a href="' + item.url + '">' + item.title + '</a>' + '</li>');
+        items.push('<li><p class="title"><a href="' + item.url + '">' + item.title + '</a> ' + 
+        '<span class="domain">(' + item.domain + ')</span></p>' +
+        '<p><span class="stats">' + item.points + ' points | ' + 
+        time_to_words( item.create_ts ) + ' ago | ' +
+        '<a href="https://news.ycombinator.com/item?id=' + item.id + '">' + 
+        item.num_comments + ' comments</a></span></p></li>');
       }
       items_list.html( items.join( '' ) );
       
@@ -70,4 +75,41 @@ function change_previous_button( activate )
     previous_button.addClass('disabled');
     previous_button_link.removeAttr('href');
   }
+}
+
+var rfc3339_date = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})Z$/;
+var msec_in_minute = 1000.0 * 60;
+var msec_in_hour = msec_in_minute * 60;
+var msec_in_day = msec_in_hour * 24;
+var msec_in_week = msec_in_day * 7;
+var msec_in_month = msec_in_day * 30;
+var msec_in_year = msec_in_day * 365;
+
+function time_to_words( datetime ) 
+{    
+  var m = rfc3339_date.exec( datetime );
+  var year   = m[1];
+  var month  = m[2];
+  var day    = m[3];
+  var hour   = m[4];
+  var minute = m[5];
+  var second = m[6];
+  var then = Date.UTC(year, month - 1, day, hour, minute, second);
+  var now = (new Date()).getTime();
+  var interval = now - then;
+  var time_ago = null;
+  if (interval > msec_in_year) {
+    time_ago = Math.round( interval / msec_in_year ) + " years";
+  } else if (interval > msec_in_month) {
+    time_ago = Math.round( interval / msec_in_month ) + " months";
+  } else if (interval > msec_in_week) {
+    time_ago = Math.round( interval / msec_in_week ) + " weeks";
+  } else if (interval > msec_in_day) {
+    time_ago = Math.round( interval / msec_in_day ) + " days";
+  } else if (interval > msec_in_hour) {
+    time_ago = Math.round( interval / msec_in_hour ) + " hours";
+  } else {
+    time_ago = Math.round( interval / msec_in_minute ) + " minutes";
+  }
+  return time_ago;
 }
